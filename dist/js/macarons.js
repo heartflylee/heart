69f195258c1eca65137859790063a55d35c31a1e
heartflylee <2595672062@qq.com>
2017-05-22 17:56:06 +0800

    修改
    Signed-off-by: heartflylee <2595672062@qq.com>

--- dist/js/macarons.js
+++ dist/js/macarons.js
@@ -1,11 +1,10 @@
-
 var theme = {
     // 默认色板
     color: [
-        '#2ec7c9','#ffb980','#d87a80','#b6a2de','#5ab1ef',
-        '#8d98b3','#e5cf0d','#97b552','#95706d','#dc69aa',
-        '#07a2a4','#9a7fd1','#588dd5','#f5994e','#c05050',
-        '#59678c','#c9ab00','#7eb00a','#6f5553','#c14089'
+        '#2ec7c9', '#ffb980', '#d87a80', '#b6a2de', '#5ab1ef',
+        '#8d98b3', '#e5cf0d', '#97b552', '#95706d', '#dc69aa',
+        '#07a2a4', '#9a7fd1', '#588dd5', '#f5994e', '#c05050',
+        '#59678c', '#c9ab00', '#7eb00a', '#6f5553', '#c14089'
     ],
 
     // 图表标题
@@ -15,31 +14,31 @@ var theme = {
             color: '#008acd'          // 主标题文字颜色
         }
     },
-    
+
     // 值域
     dataRange: {
         itemWidth: 15,
-        color: ['#5ab1ef','#e0ffff']
+        color: ['#5ab1ef', '#e0ffff']
     },
 
     // 工具箱
     toolbox: {
-        color : ['#1e90ff', '#1e90ff', '#1e90ff', '#1e90ff']
+        color: ['#1e90ff', '#1e90ff', '#1e90ff', '#1e90ff']
 //      effectiveColor : '#ff4500'
     },
 
     // 提示框
     tooltip: {
         backgroundColor: 'rgba(50,50,50,0.5)',     // 提示背景颜色，默认为透明度为0.7的黑色
-        axisPointer : {            // 坐标轴指示器，坐标轴触发有效
-            type : 'line',         // 默认为直线，可选为：'line' | 'shadow'
-            lineStyle : {          // 直线指示器样式设置
+        axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+            type: 'line',         // 默认为直线，可选为：'line' | 'shadow'
+            lineStyle: {          // 直线指示器样式设置
                 color: '#008acd'
             },
             crossStyle: {
                 color: '#008acd'
             },
-            shadowStyle : {                     // 阴影指示器样式设置
+            shadowStyle: {                     // 阴影指示器样式设置
                 color: 'rgba(200,200,200,0.2)'
             }
         }
@@ -78,10 +77,10 @@ var theme = {
                 color: '#50d2c2'
             }
         },
-        splitArea : {
-            show : true,
-            areaStyle : {
-                color: ['rgba(250,250,250,0.1)','rgba(200,200,200,0.1)']
+        splitArea: {
+            show: true,
+            areaStyle: {
+                color: ['rgba(250,250,250,0.1)', 'rgba(200,200,200,0.1)']
             }
         },
         splitLine: {           // 分隔线
@@ -91,35 +90,35 @@ var theme = {
         }
     },
 
-    polar : {
+    polar: {
         axisLine: {            // 坐标轴线
             lineStyle: {       // 属性lineStyle控制线条样式
                 color: '#ddd'
             }
         },
-        splitArea : {
-            show : true,
-            areaStyle : {
-                color: ['rgba(250,250,250,0.2)','rgba(200,200,200,0.2)']
+        splitArea: {
+            show: true,
+            areaStyle: {
+                color: ['rgba(250,250,250,0.2)', 'rgba(200,200,200,0.2)']
             }
         },
-        splitLine : {
-            lineStyle : {
-                color : '#ddd'
+        splitLine: {
+            lineStyle: {
+                color: '#ddd'
             }
         }
     },
 
-    timeline : {
-        lineStyle : {
-            color : '#008acd'
+    timeline: {
+        lineStyle: {
+            color: '#008acd'
         },
-        controlStyle : {
-            normal : { color : '#008acd'},
-            emphasis : { color : '#008acd'}
+        controlStyle: {
+            normal: {color: '#008acd'},
+            emphasis: {color: '#008acd'}
         },
-        symbol : 'emptyCircle',
-        symbolSize : 3
+        symbol: 'emptyCircle',
+        symbolSize: 3
     },
 
     // 柱形图默认参数
@@ -136,11 +135,11 @@ var theme = {
 
     // 折线图默认参数
     line: {
-        smooth : true,
+        smooth: true,
         symbol: 'emptyCircle',  // 拐点图形类型
         symbolSize: 3           // 拐点图形大小
     },
-    
+
     // K线图默认参数
     k: {
         itemStyle: {
@@ -154,7 +153,7 @@ var theme = {
             }
         }
     },
-    
+
     // 散点图默认参数
     scatter: {
         symbol: 'circle',    // 图形类型
@@ -162,9 +161,9 @@ var theme = {
     },
 
     // 雷达图默认参数
-    radar : {
+    radar: {
         symbol: 'emptyCircle',    // 图形类型
-        symbolSize:3
+        symbolSize: 3
         //symbol: null,         // 拐点图形类型
         //symbolRotate : null,  // 图形旋转控制
     },
@@ -188,65 +187,65 @@ var theme = {
             }
         }
     },
-    
-    force : {
+
+    force: {
         itemStyle: {
             normal: {
-                linkStyle : {
-                    color : '#1e90ff'
+                linkStyle: {
+                    color: '#1e90ff'
                 }
             }
         }
     },
 
-    chord : {
-        itemStyle : {
-            normal : {
+    chord: {
+        itemStyle: {
+            normal: {
                 borderWidth: 1,
                 borderColor: 'rgba(128, 128, 128, 0.5)',
-                chordStyle : {
-                    lineStyle : {
-                        color : 'rgba(128, 128, 128, 0.5)'
+                chordStyle: {
+                    lineStyle: {
+                        color: 'rgba(128, 128, 128, 0.5)'
                     }
                 }
             },
-            emphasis : {
+            emphasis: {
                 borderWidth: 1,
                 borderColor: 'rgba(128, 128, 128, 0.5)',
-                chordStyle : {
-                    lineStyle : {
-                        color : 'rgba(128, 128, 128, 0.5)'
+                chordStyle: {
+                    lineStyle: {
+                        color: 'rgba(128, 128, 128, 0.5)'
                     }
                 }
             }
         }
     },
 
-    gauge : {
+    gauge: {
         axisLine: {            // 坐标轴线
             lineStyle: {       // 属性lineStyle控制线条样式
-                color: [[0.2, '#2ec7c9'],[0.8, '#5ab1ef'],[1, '#d87a80']], 
+                color: [[0.2, '#2ec7c9'], [0.8, '#5ab1ef'], [1, '#d87a80']],
                 width: 10
             }
         },
         axisTick: {            // 坐标轴小标记
             splitNumber: 10,   // 每份split细分多少段
-            length :15,        // 属性length控制线长
+            length: 15,        // 属性length控制线长
             lineStyle: {       // 属性lineStyle控制线条样式
                 color: 'auto'
             }
         },
         splitLine: {           // 分隔线
-            length :22,         // 属性length控制线长
+            length: 22,         // 属性length控制线长
             lineStyle: {       // 属性lineStyle（详见lineStyle）控制线条样式
                 color: 'auto'
             }
         },
-        pointer : {
-            width : 5
+        pointer: {
+            width: 5
         }
     },
-    
+
     textStyle: {
         fontFamily: '微软雅黑, Arial, Verdana, sans-serif'
     }
